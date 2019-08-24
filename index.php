3b793650dae596493767641d2f8e9e92cbf8252a
a-mishra <ashutoshmishra@drishti-soft.com>
2019-08-24 20:03:01 +0530

    created possibleValuesList fn --- Json Is creating Fine now

--- index.php
+++ index.php
@@ -1,41 +1,44 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
-$DependentDropDownConfig = "References/sample custom dependent fields - Sheet1.csv";
-$returnObject = array();
+/** MasterFunction That will take in fileName and will return JSON for custom field */
+function TakeFileReturnJson( $fileName = "References/sample custom dependent fields - Sheet1.csv") {
+    $DependentDropDownConfig = $fileName;
+    $returnObject = array();
 
 
-//----converting the csvFile in multiDArray--------------------------------
-    $csvArray = array();
-    $fileOpened = fopen($DependentDropDownConfig, "r");
-    while ($fileData = fgetcsv($fileOpened)) {
-        array_push($csvArray, $fileData);
-    }
-//-------------------------------------------------------------------------
+    //----converting the csvFile in multiDArray--------------------------------
+        $csvArray = array();
+        $fileOpened = fopen($DependentDropDownConfig, "r");
+        while ($fileData = fgetcsv($fileOpened)) {
+            array_push($csvArray, $fileData);
+        }
+    //-------------------------------------------------------------------------
 
 
-//-------- creating labels for final object--------------------------------
-    $label = array();
-    for($i = 0 ; $i<count($csvArray[0]); $i++) {
-        $label[$i+1] = $csvArray[0][$i];
-    }
-    $returnObject['label']=$label;
-//--------------------------------------------------------------------------
+    //-------- creating labels for final object--------------------------------
+        $label = array();
+        for($i = 0 ; $i<count($csvArray[0]); $i++) {
+            $label[$i+1] = $csvArray[0][$i];
+        }
+        $returnObject['label']=$label;
+    //--------------------------------------------------------------------------
 
 
-//-------Label has been created now moving to values-------------------------
-    array_shift($csvArray);
-    $possibleValueList = array();
-    $returnObject['value']= array();
-    $returnObject['value']['possibleValueList'] = array();
-    $returnObject['value']['possibleValueList'] = possibleValuesList($csvArray);
-//----------------------------------------------------------------------------
+    //-------Label has been created now moving to values-------------------------
+        array_shift($csvArray);
+        $possibleValueList = array();
+        $returnObject['value']= array();
+        $returnObject['value']['possibleValueList'] = array();
+        $returnObject['value']['possibleValueList'] = possibleValuesList($csvArray);
+    //----------------------------------------------------------------------------
 
 
-//----------------------- RETUNING THE FINAL OBJECT --------------------------
-    echo (json_encode($returnObject));
-//----------------------------------------------------------------------------
-
+    //----------------------- RETUNING THE FINAL OBJECT --------------------------
+        $returnString = json_encode($returnObject);    
+        return ($returnString);
+    //----------------------------------------------------------------------------
+}
 
 //----------------------- INLINE FUNCTIONS -------------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -88,3 +91,24 @@ function possibleValuesList($mainArray) {
 }
 
 ?>
+
+
+
+<?php    
+if(isset($_POST['Submit'])){
+    $displayString = $TakeFileReturnJson();
+}    
+?>
+
+<html>
+    <body>    
+        <form action="#" method="post">
+            
+            <input type="text" name="inputText"/>
+            <input type="submit" name="Submit"/>
+
+            <?php echo $displayString; ?>
+
+        </form>    
+    </body>
+</html>
